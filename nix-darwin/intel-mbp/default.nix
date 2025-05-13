faf5dffcb5ee2c621d24208dccd8d20a89794590
Philo Wu <philo.wu@codeboyphilo.ai>
2025-05-13 18:09:13 +1000

    fix intel-mbp (@nix-darwin): fix up ssh config

--- nix-darwin/intel-mbp/default.nix
+++ nix-darwin/intel-mbp/default.nix
@@ -2,13 +2,9 @@
 {
   services.openssh = {
     enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-    };
   };
 
   users.users.${username}.openssh.authorizedKeys.keys = [
-    ./keys/hm_ed25519.pub
+    "./keys/hm_ed25519.pub"
   ];
 }
