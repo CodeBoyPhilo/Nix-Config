fb2389423378215f46855c679d64a0ab2190b200
Philo Wu <philo.wu@codeboyphilo.ai>
2025-07-26 15:39:04 +1000

    feat zsh: bind ^E to edit command in editor

--- home-manager/modules/shell/zsh/default.nix
+++ home-manager/modules/shell/zsh/default.nix
@@ -28,15 +28,22 @@
         { name = "jeffreytse/zsh-vi-mode"; }
       ];
     };
-    initContent =
-      if hostname != "vm" then
-        ''
-          export AVANTE_ANTHROPIC_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_anthropic".path})
-          export AVANTE_OPENAI_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_openai".path})
-          export AVANTE_GEMINI_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_gemini".path})
-          		''
-      else
-        "";
+    initContent = ''
+      autoload -z edit-command-line
+      zle -N edit-command-line
+      bindkey "^E" edit-command-line
+
+      ${
+        if hostname != "vm" then
+          ''
+            export AVANTE_ANTHROPIC_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_anthropic".path})
+            export AVANTE_OPENAI_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_openai".path})
+            export AVANTE_GEMINI_API_KEY=$(cat ${config.sops.secrets."api_keys/avante_gemini".path})
+          ''
+        else
+          ""
+      }
+    '';
   };
 
   programs.zoxide = {
