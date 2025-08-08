905728051ed7dde209d855ca50b98e01b7305939
Philo Wu <philo.wu@codeboyphilo.ai>
2025-08-08 14:16:56 +1000

    fix hyprland: a smoother key repeat experience

--- home-manager/modules/platforms/nixos/hyprland/default.nix
+++ home-manager/modules/platforms/nixos/hyprland/default.nix
@@ -60,6 +60,8 @@ in
         kb_layout = "us";
         follow_mouse = 1;
         sensitivity = 0;
+				repeat_rate = 40;
+				repeat_delay = 300;
       };
       general = {
         gaps_in = 5;
