f76a73235a6cf9604d0e80c6fbfbf7fdd27d63c6
Philo Wu <philo.wu@codeboyphilo.ai>
2025-08-21 12:28:50 +1000

    fix lenix: modify network setting

--- nixos/lenix/system/networking/default.nix
+++ nixos/lenix/system/networking/default.nix
@@ -8,10 +8,13 @@
   networking.hostName = hostname;
   networking.networkmanager = {
     enable = true;
-    wifi.backend = "iwd";
+    # wifi.backend = "iwd";
   };
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 22 ];
   };
+  environment.systemPackages = [
+    pkgs.networkmanagerapplet
+  ];
 }
