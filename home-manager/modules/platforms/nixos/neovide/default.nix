eed0269f603314706cb69ad9622f7559f2da354d
Philo Wu <philo.wu@codeboyphilo.ai>
2025-08-28 11:39:23 +1000

    fix lenix: separate neovide config

--- home-manager/modules/platforms/nixos/neovide/default.nix
+++ home-manager/modules/platforms/nixos/neovide/default.nix
@@ -1,32 +1,11 @@
-{ config, pkgs, ... }:
-
 {
-
-  home.packages = with pkgs; [
-    (writeShellScriptBin "neovide-with-avante" ''
-      			export AVANTE_ANTHROPIC_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_anthropic".path})"
-      			export AVANTE_OPENAI_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_openai".path})"
-      			export AVANTE_GEMINI_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_gemini".path})"
-      			exec ${pkgs.neovide}/bin/neovide "$@"
-      		'')
+  config,
+  pkgs,
+  hostname,
+  ...
+}:
+{
+  imports = [
+    ./${hostname}
   ];
-
-  programs.neovide = {
-    enable = true;
-    settings = {
-      frame = "none";
-      font = {
-        normal = [ "Maple Mono NF CN" ];
-        size = 19;
-      };
-      maximized = true;
-    };
-  };
-
-  programs.zsh = {
-    shellAliases = {
-      nv = "neovide";
-    };
-  };
-
 }
---
+++ home-manager/modules/platforms/nixos/neovide/lanix/default.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+
+  home.packages = with pkgs; [
+    (writeShellScriptBin "neovide-with-avante" ''
+      			export AVANTE_ANTHROPIC_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_anthropic".path})"
+      			export AVANTE_OPENAI_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_openai".path})"
+      			export AVANTE_GEMINI_API_KEY="$(cat ${config.sops.secrets."api_keys/avante_gemini".path})"
+      			exec ${pkgs.neovide}/bin/neovide "$@"
+      		'')
+  ];
+
+  programs.neovide = {
+    enable = true;
+    settings = {
+      frame = "none";
+      font = {
+        normal = [ "Maple Mono NF CN" ];
+        size = 19;
+      };
+      maximized = true;
+    };
+  };
+
+  programs.zsh = {
+    shellAliases = {
+      nv = "neovide";
+    };
+  };
+
+}
