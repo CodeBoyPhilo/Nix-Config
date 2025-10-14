72ca017c417ffc89a07520894419292027397076
Philo Wu <philo.wu@codeboyphilo.ai>
2025-10-14 16:37:19 +1100

    feat hm: add clock-rs

---
+++ home-manager/modules/cli-tools/add-on/clock-rs/default.nix
@@ -0,0 +1,35 @@
+{config, pkgs, ...}:
+let
+  stylix-colors = config.lib.stylix.colors.withHashtag;
+in
+{
+programs.clock-rs = {
+  enable = true;
+
+  settings = {
+    general = {
+      color = "${stylix-colors.base06}";
+      interval = 200;
+      blink = false;
+      bold = false;
+    };
+
+    position = {
+      horizontal = "center";
+      vertical = "center";
+    };
+
+    date = {
+      fmt = "%a %d %b %y";
+      use_12h = false;
+      utc = false;
+      hide_seconds = false;
+    };
+  };
+};
+  programs.zsh = {
+    shellAliases = {
+      clock = "clock-rs";
+    };
+  };
+}
